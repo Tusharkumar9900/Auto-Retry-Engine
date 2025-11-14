3030909edadc5c812cda261988c0b5dff39b6119
Tusharkumar9900 <tusharkumarr0809@gmail.com>
2025-11-14 11:01:47 +0530

    first commit

---
+++ src/test/java/com/autoretry/autoretry/AutoretryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoretry.autoretry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoretryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
